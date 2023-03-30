346b50a23094b3a994c9c71a33ba5932cbe75701
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-03-31 00:49:19 +0300

    Initialized portfolio project

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutMe from "../Components/AboutMe";
+import Heroe from "../Components/Heroe";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Heroe />
+      <AboutMe />
+    </div>
+  );
+};
+
+export default HomePage;
